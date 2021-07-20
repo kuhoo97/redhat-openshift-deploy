08bd19bb1ccc93cbec06831d87d1b7e747fa6470
pooja yadav <kuhoo97@gmail.com>
2021-07-20 11:36:01 +0530

    made  changes

--- src/main/java/com/example/demo/rest/WebController.java
+++ src/main/java/com/example/demo/rest/WebController.java
@@ -5,6 +5,7 @@ import com.example.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,7 @@ public class WebController {
 
     }
 
+    @GetMapping("/customers")
     public ResponseEntity<List<Customer>> retrieveCustomer()
     {
         return ResponseEntity.ok(customerService.retrieveCustomers());
